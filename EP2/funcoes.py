f7ef736a34f80ee1f02248edd0f15821ede581df
Mathias Zanotto <mathiassz@al.insper.edu.br>
2023-11-07 12:16:42 -0300

    funcoes corretas

--- EP2/funcoes.py
+++ EP2/funcoes.py
@@ -21,3 +21,18 @@ def inicializa(palavras):
     out['especuladas'] = espec
     out['tentativas'] = tentativas
     return out
+
+def indica_posicao(sorteada, especulada):
+    posicao = []
+    if len(sorteada) != len(especulada):
+        return posicao
+    i = 0
+    while i < len(sorteada):
+        if especulada[i] == sorteada[i]:
+            posicao.append(0)
+        elif especulada[i] in list(sorteada):
+            posicao.append(1)
+        elif especulada[i] not in list(sorteada):
+            posicao.append(2)
+        i += 1
+    return posicao
