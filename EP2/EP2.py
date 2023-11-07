0b73cd55728923780458c7b7d18bc1a4b469a9db
luigics-insper <luigics@al.insper.edu.br>
2023-11-07 15:32:33 -0300

    mais mecânicas + banco de palavras

--- EP2/EP2.py
+++ EP2/EP2.py
@@ -1,8 +1,9 @@
 from funcoes import filtra
 from funcoes import inicializa
 from funcoes import indica_posicao
+from banco_palavras import PALAVRAS
 
-palavras = ['arroz', 'menor', 'maior', 'banco', 'carro','barro']
+palavras = ['arroz', 'menor', 'maior', 'banco', 'carro','barro','banana','macaco']
 
 sorteada = inicializa(palavras)['sorteada']
 tentativas = inicializa(palavras)['tentativas']
@@ -14,14 +15,29 @@ print('Você tem',tentativas, 'para acertar uma palavra aleatória de ',qt_letra
 print('Sorteando uma palavra...\n')
 print('Você tem ',inicializa(palavras)['tentativas'],'tentativa(s)!')
 
+filtro = filtra(PALAVRAS,qt_letras)
 while espec != sorteada:
-     #print('A palavra sorteada é',sorteada,'\n')
-     espec = input('Qual é seu palpite? ')
-     especuladas.append(espec)
+     '''
+     print('A palavra sorteada é',sorteada,'\n') -- para descobrir a palavra sorteada
+     '''
+
+     espec = input('Qual é seu palpite? \n')
+
+     if espec not in PALAVRAS:
+          print('Palavra desconhecida')
+          continue
+     if espec not in filtro:
+          print('Apenas palavras de',qt_letras,'letras!\n')
+          continue
      if espec == sorteada:
           print('Voce acertou')
           break
+     if espec in especuladas:
+          print('Palavra já testada.')
+          continue
 
+     especuladas.append(espec)
+     print('\n',especuladas)
      tentativas -= 1
 
      if tentativas == 0:
