8a5238123419a09fdc5f70c057a603f830ab00f3
luigics-insper <luigics@al.insper.edu.br>
2023-11-08 16:42:43 -0300

    correção na parte de tentativas/letras informadas

--- EP2/EP2.py
+++ EP2/EP2.py
@@ -3,31 +3,27 @@ from funcoes import inicializa
 from funcoes import indica_posicao
 from banco_palavras import PALAVRAS
 
-palavras = ['arroz', 'menor', 'maior', 'banco', 'carro','barro','banana','macaco']
+palavras = palavras = ['arroz', 'menor', 'maior', 'banco', 'carro','barro','banana','macaco']
 
 sorteada = inicializa(palavras)['sorteada']
-tentativas = inicializa(palavras)['tentativas']
-qt_letras = inicializa(palavras)['n']
+tentativas = len(sorteada)+1
+qt_letras = len(sorteada)
 especuladas = []
 espec = ''
-print('Comandos: desistir, dica')
-print('Você tem',tentativas, 'para acertar uma palavra aleatória de ',qt_letras,'letras')
+
+print('Você tem',tentativas, 'tentativas para acertar uma palavra aleatória de',qt_letras,'letras')
 print('Sorteando uma palavra...\n')
-print('Você tem ',inicializa(palavras)['tentativas'],'tentativa(s)!')
+print('Você tem',tentativas,'tentativa(s)!')
 
 filtro = filtra(PALAVRAS,qt_letras)
+
 while espec != sorteada:
      '''
      print('A palavra sorteada é',sorteada,'\n') -- para descobrir a palavra sorteada
      '''
 
      espec = input('Qual é seu palpite? \n')
-     if espec == 'desistir':
-          confirmar = input('Tem certeza que deseja desistir? (s/n)')
-          if confirmar == 's':
-               print('A palavra era:',sorteada)
-               break
-          continue
+
      if espec not in PALAVRAS:
           print('Palavra desconhecida')
           continue
